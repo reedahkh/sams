d9f1c71600e889888369820c3c0b5076bff21463
Chidi Okoye <chidizy4@yahoo.com>
2018-03-18 19:53:02 +0100

    Website Completely Delivered

---
+++ js/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    //testimonial carousel
+    $(".testimonial-carousel").slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        arrows: false,
+        autoplaySpeed: 6000,
+        prevArrow: $(".testimonial-carousel-controls .prev"),
+        nextArrow: $(".testimonial-carousel-controls .next")
+    });
+    // big video background
+    // $(function() {
+    //     var BV = new $.BigVideo({container: $('#video_bg')});
+
+    //     BV.init();
+    //     BV.getPlayer().pause();
+
+    //     BV.show('video/SAMS_Advert.mp4', {doLoop:true}, {autoplay:true});
+
+    // });
+    // features card toggle
+    $(".card_container").mouseover(function(){
+        $(this).find(".side_2").show ()
+    });
+    $(".card_container").mouseleave(function(){
+        $(this).find(".side_2").hide();
+    });
+
+});
