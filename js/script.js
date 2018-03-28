04d0eb56f60a32ff9da81d6763b2266b7ac4a2fa
Chidi Okoye <chidizy4@yahoo.com>
2018-03-28 06:55:33 +0100

    Dasboard

--- js/script.js
+++ js/script.js
@@ -11,16 +11,17 @@ $(document).ready(function() {
         prevArrow: $(".testimonial-carousel-controls .prev"),
         nextArrow: $(".testimonial-carousel-controls .next")
     });
+
     // big video background
-    // $(function() {
-    //     var BV = new $.BigVideo({container: $('#video_bg')});
+   /* $(function() {
+        var BV = new $.BigVideo({container: $('#video_bg')});
 
-    //     BV.init();
-    //     BV.getPlayer().pause();
+        BV.init();
+        BV.getPlayer().pause();
 
-    //     BV.show('video/SAMS_Advert.mp4', {doLoop:true}, {autoplay:true});
-
-    // });
+        BV.show('../video/SAMS_Advert.mp4', {doLoop:true}, {autoplay:true})
+    });*/
+        
     // features card toggle
     $(".card_container").mouseover(function(){
         $(this).find(".side_2").show ()
